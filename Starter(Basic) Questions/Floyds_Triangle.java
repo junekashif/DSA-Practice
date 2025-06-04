988dd4563a1d85ad85dd2e38bb3e90a9495216d2
Kashif <kakashi.f.28th@gmail.com>
2025-06-04 23:37:20 +0530

    create Floyds Triangle Problem
    done floyds triangle pattern problem .

---
+++ Starter(Basic) Questions/Floyds_Triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Floyds_Triangle
+{
+    public static void main (String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number ");
+        int n=sc.nextInt(),p=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+            System.out.print(p+" ");
+            p++;
+            }
+            System.out.println();
+        }
+    }
+}
