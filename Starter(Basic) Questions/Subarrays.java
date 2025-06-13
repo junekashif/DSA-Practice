7a009b290f2cfeb4960878a77295d82e2ba872f0
Kashif <kakashi.f.28th@gmail.com>
2025-06-13 23:52:00 +0530

    Update Subarrays.java
    done subarray modifications

--- Starter(Basic) Questions/Subarrays.java
+++ Starter(Basic) Questions/Subarrays.java
@@ -5,8 +5,9 @@ class Subarrays
         int n=ar.length;
         for(int i=0;i<n;i++)
         {
-            for(int j=i;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
+                int s=0;
                 for(int k=i;k<=j;k++)
                 {
                     System.out.print(ar[k]+" ");
