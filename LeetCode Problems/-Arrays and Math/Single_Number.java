e1ce43596f52681e0235618bdfeb2e18b11acf81
Kashif <kakashi.f.28th@gmail.com>
2025-06-27 23:57:45 +0530

    Add solution for Single Number problem
    Implements a method to find the single non-duplicate number in an array using XOR operation.

---
+++ LeetCode Problems/-Arrays and Math/Single_Number.java
@@ -0,0 +1,13 @@
+class Single_Number 
+{
+  public int singleNumber(int[] nums) 
+    {
+        int index=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            index=index^nums[i];
+        }
+        
+        return index;
+    }  
+}
