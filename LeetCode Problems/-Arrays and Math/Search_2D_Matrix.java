ab75bbc38f4c9147bf549e9850f947d2d2b3b9db
Kashif <kakashi.f.28th@gmail.com>
2025-07-09 23:44:20 +0530

    Add solution for Search 2D Matrix problem
    Implements a method to search for a target value in a 2D matrix using an efficient approach starting from the top-right corner. Returns true if the target is found, otherwise returns false.

---
+++ LeetCode Problems/-Arrays and Math/Search_2D_Matrix.java
@@ -0,0 +1,29 @@
+public class Search_2D_Matrix 
+{
+    public boolean searchMatrix(int[][] matrix, int target) 
+    {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int i = 0;
+        int j = cols - 1;
+
+        while (i < rows && j >= 0) 
+        {
+            int element = matrix[i][j];
+            if (element == target) 
+            {
+                return true;
+            } 
+            else if (element > target) 
+            {
+                j--;
+            } 
+            else 
+            {
+                i++;
+            }
+        }
+
+        return false;
+    }
+}
