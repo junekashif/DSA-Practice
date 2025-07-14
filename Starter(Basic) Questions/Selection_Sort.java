f2204de9baad312be9b56d26527fbba72ebcb7c5
Kashif <kakashi.f.28th@gmail.com>
2025-07-14 22:12:20 +0530

    Add selection sort implementation in Java
    Introduces a Selection_Sort class with methods to perform selection sort and print an array. The main method demonstrates sorting an example array.

---
+++ Starter(Basic) Questions/Selection_Sort.java
@@ -0,0 +1,32 @@
+class Selection_Sort
+{
+    public static void SelectSort(int ar[])
+    {
+        int len=ar.length;
+        for(int i=0;i<len-1;i++)
+        {
+            int minpos=i;
+            for(int j=i+1;j<len;j++)
+            {
+                if(ar[minpos]<ar[j])
+                minpos=j;
+            }
+            int temp = ar[i];
+            ar[i] = ar[minpos];
+            ar[minpos]=temp;
+        }
+    }
+    public static void printArr(int ar[])
+    {
+        for(int i=0;i<ar.length;i++)
+        {
+            System.out.print(ar[i]+" ");
+        }
+    }
+    public static void main(String arg[])
+    {
+        int arr[]={5,2,1,6,4,4};
+        SelectSort(arr);
+        printArr(arr);
+    }
+}
